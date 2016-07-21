21ff81ee4b878130a8b3311590ca383d4c9d4509
Cassiano Honorio <honoriocassiano@gmail.com>
2016-07-21 17:12:21 -0300

    Alteração da cor de fundo

--- src/SDLWindow.cpp
+++ src/SDLWindow.cpp
@@ -151,7 +151,8 @@ void SDLWindow::processRealtimeEvents() {
 }
 
 void SDLWindow::display() {
-	glClearColor(1, 1, 1, 0);
+	//glClearColor(1, 1, 1, 0);
+	glClearColor(0, 0, 0, 0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_MODELVIEW);
