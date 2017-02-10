72911cc225b8d83b3fa03c16770b472fa4b9d572
Cassiano Honorio <honoriocassiano@gmail.com>
2017-02-10 08:16:29 -0200

    Identação de arquivos

--- src/structures/Halfedge/vertex_parent.h
+++ src/structures/Halfedge/vertex_parent.h
@@ -18,42 +18,48 @@
 
 // ===================================================================
 
-class VertexParent { 
+class VertexParent {
 
 public:
 
-  // ========================
-  // CONSTRUCTORS & DESTRUCTOR
-  VertexParent(Vertex *_p1, Vertex *_p2, Vertex *_v) {
-    assert (_p1 != NULL && _p2 != NULL && _v != NULL);
-    assert (_p1 != _p2 && _p1 != _v && _p2 != _v);
-    p1 = _p1;
-    p2 = _p2;
-    v = _v;
-  }
-  ~VertexParent();
-  
-  // to be put in a bag...
-  static void extract_func(VertexParent *e, int &a, int &b, int &c) {
-    a = min2(e->p1->getIndex(),e->p2->getIndex());
-    b = max2(e->p1->getIndex(),e->p2->getIndex());
-    c = 0;
-  }
- 
-  // =========
-  // ACCESSORS
-  Vertex* get() const { return v; }
+	// ========================
+	// CONSTRUCTORS & DESTRUCTOR
+	VertexParent(Vertex *_p1, Vertex *_p2, Vertex *_v) {
+		assert(_p1 != NULL && _p2 != NULL && _v != NULL);
+		assert(_p1 != _p2 && _p1 != _v && _p2 != _v);
+		p1 = _p1;
+		p2 = _p2;
+		v = _v;
+	}
+	~VertexParent();
+
+	// to be put in a bag...
+	static void extract_func(VertexParent *e, int &a, int &b, int &c) {
+		a = min2(e->p1->getIndex(), e->p2->getIndex());
+		b = max2(e->p1->getIndex(), e->p2->getIndex());
+		c = 0;
+	}
+
+	// =========
+	// ACCESSORS
+	Vertex* get() const {
+		return v;
+	}
 
 protected:
 
-  VertexParent(const VertexParent&) { assert(0); }
-  VertexParent& operator=(const VertexParent&) { assert(0); }
+	VertexParent(const VertexParent&) {
+		assert(0);
+	}
+	VertexParent& operator=(const VertexParent&) {
+		assert(0);
+	}
 
-  // ==============
-  // REPRESENTATION
-  Vertex *p1;
-  Vertex *p2;
-  Vertex *v;
+	// ==============
+	// REPRESENTATION
+	Vertex *p1;
+	Vertex *p2;
+	Vertex *v;
 };
 
 // ===================================================================
